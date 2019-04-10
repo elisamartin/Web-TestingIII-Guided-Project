a0301fc1349acab559429feb81defb9708112a83
elisamartin <elisamv93@gmail.com>
2019-04-10 14:33:01 +0200

    lecture wip

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,48 +2,57 @@ import React from 'react';
 import axios from 'axios';
 
 export default class Login extends React.Component {
-  state = { flashMessage: '', username: '', password: '' }
+	state = { flashMessage: '', username: '', password: '' };
 
-  onChange = (field, value) => {
-    this.setState({ [field]: value });
-  }
+	onChange = (field, value) => {
+		this.setState({ [field]: value });
+	};
 
-  onLogin = () => {
-    axios.post('login', {
-      username: this.state.username,
-      password: this.state.password,
-    })
-      .then(res => {
-        localStorage.setItem('token', res.data.token);
-        this.setState({ flashMessage: `Welcome, ${res.data.username}` });
-      })
-      .catch(res => {
-        this.setState({ flashMessage: res.message });
-      });
-  }
+	onLogin = () => {
+		axios
+			.post('login', {
+				username: this.state.username,
+				password: this.state.password
+			})
+			.then((res) => {
+				localStorage.setItem('token', res.data.token);
+				this.setState({ flashMessage: `Welcome, ${res.data.username}` });
+			})
+			.catch((res) => {
+				this.setState({ flashMessage: res.message });
+			});
+	};
 
-  render() {
-    return (
-      <div>
-        <div>
-          username <input
-            className='input'
-            onChange={e => this.onChange('username', e.target.value)}
-            placeholder='username'
-            type="text"
-          /> <br />
-          password <input
-            className='input'
-            onChange={e => this.onChange('password', e.target.value)}
-            placeholder='password'
-            type="text"
-          />
-        </div>
-        <button onClick={this.onLogin}>
-          Login
-        </button>
-        <div>{this.state.flashMessage}</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div>
+					<label htmlFor="usernameInput">username</label>
+					<input
+						id="usernameInput"
+						className="input"
+						onChange={(e) => this.onChange('username', e.target.value)}
+						placeholder="username"
+						type="text"
+					/>
+					<br />
+					<label htmlFor="passwordInput">password</label>
+					<input
+						id="passwordInput"
+						className="input"
+						onChange={(e) => this.onChange('password', e.target.value)}
+						placeholder="password"
+						type="text"
+					/>
+				</div>
+				{this.state.username &&
+				this.state.password && (
+					<button onClick={this.onLogin} data-testid="loginButton">
+						Login
+					</button>
+				)}
+				<div>{this.state.flashMessage}</div>
+			</div>
+		);
+	}
 }
--- src/components/Login.test.jsx
+++ src/components/Login.test.jsx
@@ -5,20 +5,80 @@ import Login from './Login';
 afterEach(rt.cleanup);
 
 describe('Login', () => {
-  it('displays no login button unless username & password', () => {
-    // write test first, see it fail, implement feature, see test pass
-  });
+	it('displays no login button if no username or no password', () => {
+		const wrap = rt.render(<Login />);
+		const button = wrap.queryByTestId(/loginButton/i);
+		expect(button).toBeFalsy();
+		expect(wrap.asFragment()).toMatchSnapshot();
+	});
 
-  it('can change input values', () => {
-    // we can fire change events on inputs
-    // and we can grab inputs by their placeholder texts or their values
-  });
+	// it('can change input values', () => {
+	//   const wrap = rt.render(<Login />);
+	//   rt.fireEvent.change(
+	//     wrap.getByPlaceholderText('username'),
+	//     { target: { value: 'foo' } },
+	//   );
 
-  it('can login successfully', async () => {
-    // see the greeting render
-  });
+	//   rt.fireEvent.change(
+	//     wrap.getByPlaceholderText('password'),
+	//     { target: { value: 'bar' } },
+	//   );
 
-  it('can fail miserably', async () => {
-    // see the error render
-  });
+	//   expect(wrap.getByDisplayValue('foo'));
+	//   expect(wrap.getByDisplayValue('bar'));
+	// });
+
+	it('can change input values', () => {
+		const wrap = rt.render(<Login />);
+		expect(wrap.asFragment()).toMatchSnapshot();
+
+		const usernameInput = wrap.getByPlaceholderText('username');
+		const passwordInput = wrap.getByPlaceholderText('password');
+
+		const inputValue = 'name';
+		const passValue = 'secretPassword';
+
+		rt.fireEvent.change(usernameInput, { target: { value: inputValue } });
+		expect(wrap.getByPlaceholderText('username').value).toBe(inputValue);
+
+		rt.fireEvent.change(passwordInput, { target: { value: passValue } });
+		expect(wrap.getByPlaceholderText('password').value).toBe(passValue);
+	});
+
+	it('displays login button if username and password', () => {
+		const wrap = rt.render(<Login />);
+		const usernameInput = wrap.getByLabelText('username');
+		const passwordInput = wrap.getByLabelText('password');
+
+		rt.fireEvent.change(usernameInput, { target: { value: 'A' } });
+
+		expect(wrap.queryByTestId(/loginButton/i)).toBeFalsy();
+
+		rt.fireEvent.change(passwordInput, { target: { value: 'B' } });
+
+		expect(wrap.queryByTestId(/loginButton/i)).toBeTruthy();
+	});
+
+	it('can login successfully', async () => {
+		// grab the component
+		const wrap = rt.render(<Login />);
+		// change username to Alex
+		rt.fireEvent.change(wrap.getByPlaceholderText('username'), {
+			target: { value: 'Alex' }
+		});
+		// change password to be longer than 0
+		rt.fireEvent.change(wrap.getByLabelText('password'), {
+			target: { value: 'secret' }
+		});
+		// click the button
+		rt.fireEvent.click(wrap.queryByTestId(/loginButton/i));
+		//await flash msg to appear
+		await wrap.findByText(/welcome/i);
+		// assert message
+		expect(wrap.getByText(/welcome/i));
+	});
+
+	it('can fail miserably', async () => {
+		// see the error render
+	});
 });
